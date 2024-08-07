c386833fb619fcbe4a53682c61be9b79d72fc6b4
huseyinyasarr <h.yasar.tr@gmail.com>
2024-08-07 12:24:17 +0300

    ValidationFilter added

---
+++ src/Api/WebApi/BlazorSozluk.Api.WebApi/Infrastructure/Extensions/AuthRegistration.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.IdentityModel.Tokens;
+using System.Text;
+
+namespace BlazorSozluk.Api.WebApi.Infrastructure.Extensions;
+
+public static class AuthRegistration
+{
+    public static IServiceCollection ConfigureAuth(this IServiceCollection services, IConfiguration configuration)
+    {
+        var singingKey = new SymmetricSecurityKey(Encoding.ASCII.GetBytes(configuration["Jwt:SigningKey"]));
+    }
+}
