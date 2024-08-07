452df29cfc78c671bc2a872d7d8e4e99399126c1
huseyinyasarr <h.yasar.tr@gmail.com>
2024-08-07 12:52:32 +0300

    AuthRegistration added

--- src/Api/WebApi/BlazorSozluk.Api.WebApi/Infrastructure/Extensions/AuthRegistration.cs
+++ src/Api/WebApi/BlazorSozluk.Api.WebApi/Infrastructure/Extensions/AuthRegistration.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.IdentityModel.Tokens;
+﻿using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
 using System.Text;
 
 namespace BlazorSozluk.Api.WebApi.Infrastructure.Extensions;
@@ -7,6 +8,25 @@ public static class AuthRegistration
 {
     public static IServiceCollection ConfigureAuth(this IServiceCollection services, IConfiguration configuration)
     {
-        var singingKey = new SymmetricSecurityKey(Encoding.ASCII.GetBytes(configuration["Jwt:SigningKey"]));
+        var signingKey = new SymmetricSecurityKey(Encoding.ASCII.GetBytes(configuration["Jwt:SigningKey"]));
+
+        services.AddAuthentication(options =>
+        {
+            options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+            options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+        })
+            .AddJwtBearer(options =>
+            {
+                options.TokenValidationParameters = new TokenValidationParameters()
+                {
+                    ValidateIssuer = false,
+                    ValidateAudience = false,
+                    ValidateLifetime = true,
+                    ValidateIssuerSigningKey = true,
+                    IssuerSigningKey = signingKey
+
+                };
+            });
+        return services;
     }
 }
