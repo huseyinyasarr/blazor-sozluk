30814546739c0ce7df0dc42582c3bf459a8f7beb
huseyinyasarr <h.yasar.tr@gmail.com>
2024-08-19 10:31:03 +0300

    updated

--- src/Projections/BlazorSozluk.Projections.UserService/Services/UserService.cs
+++ src/Projections/BlazorSozluk.Projections.UserService/Services/UserService.cs
@@ -1,12 +1,38 @@
-﻿using System;
+﻿using BlazorSozluk.Common.Events.User;
+using Dapper;
+using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace BlazorSozluk.Projections.UserService.Services
+namespace BlazorSozluk.Projections.UserService.Services;
+public class UserService
 {
-    internal class UseerService
+    private string connStr;
+
+    public UserService(IConfiguration configuration)
     {
+        connStr = configuration.GetConnectionString("SqlServer");
     }
+
+    public async Task<Guid> CreateEmailConfirmation(UserEmailChangedEvent @event)
+    {
+        var guid = Guid.NewGuid();
+
+
+        using var connection = new SqlConnection(connStr);
+
+        await connection.ExecuteAsync("INSERT INTO EMAILCONFIRMATION (Id, CreateDate, OldEmailAddress, NewEmailAddress) VALUES (@Id, GETDATE(), @OldEmailAddress, @NewEmailAddress)",
+                new
+                {
+                    Id = guid,
+                    OldEmailAddress = @event.OldEmailAddress,
+                    NewEmailAddress = @event.NewEmailAddress
+                });
+
+        return guid;
+    }
+
 }
